2b90dcf7238b0659fc667c61ad4f3b0df6277005
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2023-08-08 16:10:26 +0800

    Update watch.js

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -20,7 +20,7 @@ const tlt = document.querySelector('title');
 
 $(document).ready(function () {
     $.ajax({
-            url: `https://senpai-api.vercel.app/getVideo/player?id=${id}`,
+            url: `https://hentaimama.io/getVideo/player?id=${id}`,
             type: "GET",
             dataType: "json",
             success: async function (data) {
@@ -29,7 +29,7 @@ $(document).ready(function () {
             }
         })
     $.ajax({
-        url: `https://senpai-api.vercel.app/getInfo?id=${id}`,
+        url: `https://hentaimama.io/getInfo?id=${id}`,
         type: "GET",
         dataType: "json",
         success: async function (data) {
