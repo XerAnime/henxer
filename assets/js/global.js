0971d74f0d0000873b82c9b23d1dd515a52286de
Animxer18 <138975524+Animxer18@users.noreply.github.com>
2023-08-08 16:05:31 +0800

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,7 +3,7 @@ const api_version = document.getElementById('api_v');
 //check if the api_v is there
 
 $.ajax({
-    url: 'https://senpai-api.vercel.app',
+    url: 'https://hentaimama.io/',
     type: 'GET',
     dataType: 'json',
     success: async function (data) {
